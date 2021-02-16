d4af08aae35bc1a942eef7a602ee1c721029558c
Ruud Hermans <info@ruud-hermans.nl>
2021-02-16 10:48:34 +0100

    stashissue resolve

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function create(){
+
+    }
+
+
+    public function store(){
+        if(//authorization
+            ){ 
+                
+            } else {
+
+            }
+    }
+
+
+}
