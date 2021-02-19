3fe4d40ba963d52fd0bcdc02bb5f6e88349f6d5d
Ruud Hermans <info@ruud-hermans.nl>
2021-02-19 13:14:16 +0100

    usercontroller + frontend main page werk

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -3,10 +3,18 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+use App\Models\User;
 
 class UserController extends Controller
 {
     public function index(){
-        return view('user/profile');
+        
+        $user = Auth::user();
+
+        return view('user/profile',
+            
+        );
     }
 }
