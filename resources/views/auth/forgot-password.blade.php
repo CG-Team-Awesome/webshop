1434a5fdffb268855e934352501811d891e385a4
Maurice di Tolve <maurice_di_tolve@hotmail.com>
2021-02-22 15:58:32 +0100

    22/02 Maurice Navbar Dingetjes En Login/Register

--- resources/views/auth/forgot-password.blade.php
+++ resources/views/auth/forgot-password.blade.php
@@ -1,40 +1,40 @@
-<x-guest-layout>
-    <x-auth-card>
-        <x-slot name="logo">
-            <a href="/">
-                <x-application-logo width="82" />
-            </a>
-        </x-slot>
+<div class="container-fluid forgot-div">
+    <x-guest-layout>
+        <x-auth-card>
+            <x-slot name="logo">
+                <a href="/">
+                    <x-application-logo width="82" />
+                </a>
+            </x-slot>
 
-        <div class="card-body">
-            <div class="mb-4">
-                {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+            <div class="card-body forgot-card shadow-mau">
+                <div class="mb-4">
+                    {{ __('Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.') }}
+                </div>
+
+                <div class="card-body">
+                    <!-- Session Status -->
+                    <x-auth-session-status class="mb-3" :status="session('status')" />
+
+                    <!-- Validation Errors -->
+                    <x-auth-validation-errors class="mb-3" :errors="$errors" />
+
+                    <form method="POST" action="{{ route('password.email') }}">
+                    @csrf
+
+                    <!-- Email Address -->
+                        <div class="form-group">
+                            <x-label for="email" :value="__('Email')" />
+
+                            <x-input id="email" type="email" name="email" :value="old('email')" required autofocus />
+                        </div>
+
+                        <div class="d-flex justify-content-end mt-4">
+                            <a href="{{ __('Email Password Reset Link') }}"><button class="btn btn-mau shadow-mau mt-3">Email Password Reset Link</button></a>
+                        </div>
+                    </form>
+                </div>
             </div>
-
-            <div class="card-body">
-                <!-- Session Status -->
-                <x-auth-session-status class="mb-3" :status="session('status')" />
-
-                <!-- Validation Errors -->
-                <x-auth-validation-errors class="mb-3" :errors="$errors" />
-
-                <form method="POST" action="{{ route('password.email') }}">
-                @csrf
-
-                <!-- Email Address -->
-                    <div class="form-group">
-                        <x-label for="email" :value="__('Email')" />
-
-                        <x-input id="email" type="email" name="email" :value="old('email')" required autofocus />
-                    </div>
-
-                    <div class="d-flex justify-content-end mt-4">
-                        <x-button>
-                            {{ __('Email Password Reset Link') }}
-                        </x-button>
-                    </div>
-                </form>
-            </div>
-        </div>
-    </x-auth-card>
-</x-guest-layout>
+        </x-auth-card>
+    </x-guest-layout>
+</div>
--- resources/views/partials/mainnavbar.blade.php
+++ resources/views/partials/mainnavbar.blade.php
@@ -4,12 +4,14 @@
             @if (Route::has('login'))
             <div class="">
                 @auth
-                <a class="title-font" href="{{ url('/dashboard') }}">Dashboard</a>
+                <a class="mr-5" href="{{ url('/cart') }}"><img src="images/shopping-cart.svg" class="cart-icon" alt="cart svg"></a>
+                <a class="title-font mr-4 navhover" href="{{ url('/profile') }}">Profiel</a>
+                <a href="{{ url('/logout') }}"><img src="images/exit.svg" alt="logout svg" class="logout-icon"></a>
                 @else
-                <a class="title-font mr-1" href="{{ route('login') }}">Login</a>
+                <a class="title-font mr-1 navhover" href="{{ route('login') }}">Login</a>
 
                 @if (Route::has('register'))
-                <a class="title-font ml-3" href="{{ route('register') }}">Register</a>
+                <a class="title-font ml-3 navhover" href="{{ route('register') }}">Register</a>
                 @endif
                 @endif
             </div>
