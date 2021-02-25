10cfd6f7bebb2928384ea6bd3e154481ed1f9f1d
xmhag <xanderhageman@outlook.com>
2021-02-25 17:22:17 +0100

    admin basic setup

--- database/seeders/CreateAdminUserSeeder.php
+++ database/seeders/CreateAdminUserSeeder.php
@@ -1,12 +1,12 @@
 <?php
-  
+
 namespace Database\Seeders;
-  
+
 use Illuminate\Database\Seeder;
 use App\Models\User;
 use Spatie\Permission\Models\Role;
 use Spatie\Permission\Models\Permission;
-  
+
 class CreateAdminUserSeeder extends Seeder
 {
     /**
@@ -17,17 +17,19 @@ class CreateAdminUserSeeder extends Seeder
     public function run()
     {
         // $user = User::create([
-        //     'first_name' => 'Admin', 
+        //     'first_name' => 'Admin',
         //     'email' => 'admin@admin.com',
         //     'password' => Hash::make('password'),
         // ]);
-    
-        $role = Role::create(['name' => 'Admin']);
-     
-        $permissions = Permission::pluck('id','id')->all();
-   
-        $role->syncPermissions($permissions);
-     
+
+        $role = Role::create(['name' => 'admin']);
+        $role = Role::create(['name' => 'klant']);
+
+
+        // $permissions = Permission::pluck('id','id')->all();
+
+        // $role->syncPermissions($permissions);
+
         // $user->assignRole([$role->id]);
     }
 }
